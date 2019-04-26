554f04954e6458e56b1054eea61bb4d85602df52
mathus101 <mattjamesbrooks@gmail.com>
2019-04-26 10:24:10 +1000

    Add project files.

---
+++ WebApiReact/ClientApp/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+import { Layout } from './components/Layout';
+import { Home } from './components/Home';
+import { FetchData } from './components/FetchData';
+import { Counter } from './components/Counter';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner, faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faSpinner, faCoffee)
+
+export default class App extends Component {
+  static displayName = App.name;
+
+  render () {
+    return (
+      <Layout>
+        <Route exact path='/' component={Home} />
+        <Route path='/counter' component={Counter} />
+        <Route path='/fetch-data' component={FetchData} />
+      </Layout>
+    );
+  }
+}
